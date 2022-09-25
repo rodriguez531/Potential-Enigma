7b6163b5163a8a58f4d7377619c66da8c3a314b4
rodriguez531 <alexr5408@gmail.com>
2022-09-25 12:53:12 -0400

    first commit

---
+++ Develop/NewREADME.md
@@ -0,0 +1,35 @@
+# max
+
+  ## Description
+  max
+
+  ##Table of Contents
+  [Discription](#discription)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contribution](#contribution)
+  [Test](#test)
+  [License](#license)
+  [Questions](#questions)
+
+
+  ## Installation
+  max
+
+  ## Usage
+  max
+
+  ## Contributions
+  max
+
+  ## Test
+  max
+
+  ## License
+  MIT,APACHE,No license
+
+  ## Questions
+  https://github.com/max
+  max
+
+
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,56 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+let license = "";
+
+if(license )
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ##Table of Contents
+  [Discription](#discription)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Contribution](#contribution)
+  [Test](#test)
+  [License](#license)
+  [Questions](#questions)
+
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributions
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## License
+  ${data.license}
+
+  ## Questions
+  https://github.com/${data.github}
+  ${data.email}
+
+
+`;
+}
+
+module.exports = generateMarkdown;
